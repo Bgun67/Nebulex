f014c28454df95a7b7687347891c5ffd1843c2bd
oasis-mihal <oasis.mihal@gmail.com>
2019-04-26 22:18:12 -0400

    Compressed Air

--- Assets/Scripts/Players/Player_Controller.cs
+++ Assets/Scripts/Players/Player_Controller.cs
@@ -1103,8 +1103,9 @@ public class Player_Controller : MonoBehaviour {
 			}
 
 			//Play thruster sounds
-			walkSound.PlayOnShot(thrusterClips [0], Math.Clamp01(Math.Abs(Input.GetAxis("Move X") + Input.GetAxis("Move Y") + Input.GetAxis("Move Z")); 
-
+			if(!walkSound.isPlaying){
+				walkSound.PlayOneShot(thrusterClips[0], Mathf.Clamp01(Mathf.Abs(Input.GetAxis("Move X") + Input.GetAxis("Move Y") + Input.GetAxis("Move Z"))));
+			}
 
 			Vector3 _rotateAmount = Vector3.Lerp(previousRot,new Vector3(-v2 * 0.75f, h2*2.5f, -h * 1.5f) * 5f * Time.deltaTime * 30f, 0.05f);
 			rb.transform.Rotate(_rotateAmount);
