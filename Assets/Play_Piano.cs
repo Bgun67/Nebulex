b53195dfee80e9d066b696062531fad086c8773d
Braden Gunther <bradengunther@cmail.carleton.ca>
2019-05-12 19:39:20 -0400

    Ray Charles

---
+++ Assets/Play_Piano.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Play_Piano : MonoBehaviour
+{
+	Animator anim;
+	// Start is called before the first frame update
+	void Start()
+    {
+		anim = GetComponent<Animator>();
+	}
+
+    // Update is called once per frame
+    void OnCollisionEnter( Collision other)
+    {
+
+		if (other.transform.tag == "Bullet")
+		{
+			anim.SetTrigger("Play");
+			print(-Vector3.SignedAngle(transform.forward, -other.relativeVelocity, transform.up) );
+			anim.SetFloat("Blend",-Vector3.SignedAngle(transform.forward, -other.relativeVelocity, transform.up));
+		}
+	}
+}
