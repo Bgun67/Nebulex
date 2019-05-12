afce85c8e23483c01cd0d1738f0f07eb44fb824f
oasis-mihal <oasis.mihal@gmail.com>
2019-05-11 23:26:42 -0400

    New splash screen

--- Assets/Scripts/Game Management/Splash_Scene_Controller.cs
+++ Assets/Scripts/Game Management/Splash_Scene_Controller.cs
@@ -5,12 +5,20 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityEngine.Video;
 
+
 public class Splash_Scene_Controller : MonoBehaviour {
 	public Text startText;
+	public Image shadow;
+	float initialTime = 0;
 
 	// Use this for initialization
 	void Start () {
-		Invoke("NextScene", 6f);
+		if(SceneManager.GetActiveScene().name == "DiPolar Scene"){
+				Invoke("NextScene", 6f);
+		}
+		
+		initialTime = Time.time;
+		
 		
 	}
 	
@@ -20,16 +28,22 @@ public class Splash_Scene_Controller : MonoBehaviour {
 			if(SceneManager.GetActiveScene().name == "DiPolar Scene"){
 				SceneManager.LoadScene ("Splash Scene");
 			}
-			else{
-				SceneManager.LoadScene ("Start Scene");
-			}
+			
 		}
+		if(SceneManager.GetActiveScene().name == "Splash Scene"){
+			shadow.color = new Color(0,0,0, Mathf.Clamp01(1.0f - (Time.time - initialTime) * 0.333f));
+		}
+
 	}
 
-	void NextScene(){
+	public void NextScene(){
 		if(SceneManager.GetActiveScene().name == "DiPolar Scene"){
 				SceneManager.LoadScene ("Splash Scene");
 		}
+		else{
+			SceneManager.LoadScene ("Start Scene");
+		}
 	}
+	
 	
 }
