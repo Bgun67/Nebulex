3173511e12e1ee9347aaad6f07cd52fad20321a4
Braden Gunther <bradengunther@cmail.carleton.ca>
2022-01-02 18:09:38 -0500

    Imported Suit, Scope bug active

--- Assets/Scripts/Players/Com_Controller.cs
+++ Assets/Scripts/Players/Com_Controller.cs
@@ -81,7 +81,7 @@ public class Com_Controller : NetworkBehaviour {
 		//gameController.bots.Add(this);
 		if (head == null)
 		{
-			head = GameObject.Find("Main Camera").transform;
+			head = GameObject.Find("Player Camera").transform;
 		}
 
 		anim.SetFloat("Look Speed", 0.5f);
